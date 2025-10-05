85a4940adbbfab312dd50b30741dc26f460e805f
Guseyn406 <guseyn99912@gmail.com>
2025-10-05 12:08:51 +0500

    next

---
+++ task/array.js
@@ -0,0 +1,13 @@
+let arr = ["pillow","home","killer"]
+delete arr[1];
+
+console.log(arr.length)
+
+
+
+let users = [
+    {id:1, name: "Hasan"},
+    {id: 2, name: "Husen"},
+    {id: 3, name: "Bill"},
+];
+console.log(users.findIndex(id1))
---
+++ task/varlet.js
@@ -0,0 +1,44 @@
+// let a = 40;
+// {
+//     console.log(a)
+//     var k = 90;
+// }
+// console.log(k)
+
+
+
+
+// let b = 80;
+// {
+//     console.log(b)
+//     var i = 90;
+// }
+// console.log(i)
+
+// let x = 50
+// let y = 60
+// let x1 = 4 
+// let x2 = 5
+// {
+//     console.log(x)
+//     console.log(y)
+//     console.log(x2)
+// }
+// console.log(x1)
+
+
+
+
+
+
+
+let x = 5555;
+{
+    let y = 50;
+    let yy  = 80
+    console.log(x)
+    console.log(y)
+    {
+        console.log(yy)
+    }
+}
